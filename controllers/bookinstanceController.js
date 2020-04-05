385bbcd474e3492974329b03efef57e9ec09e682
Choyoung Yim <alperceire@pm.me>
2020-04-04 22:20:37 -0300

    implement till genre details

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -4,12 +4,12 @@ var BookInstance = require('../models/bookinstance');
 exports.bookinstance_list = function(req, res, next) {
 
     BookInstance.find()
-      .populate('book')
-      .exec(function (err, list_bookinstances) {
-        if (err) { return next(err); }
-        // Successful, so render
-        res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
-      });
+        .populate('book')
+        .exec(function (err, list_bookinstances) {
+            if (err) { return next(err); }
+            // Successful, so render
+            res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
+        });
 
 };
 
